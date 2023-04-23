a4e5d155da0a22768f7e34ad6cab862a7d0b5bb3
ichraa99 <ichraa99@gmail.com>
2023-04-23 12:59:04 +0000

    fix design bugs on mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
     $(".navbar.navbar-light .navbar-nav .nav-item .nav-link div div .icon-default").css({ "stroke": "red" })
     nav = $(".nav-item")
+    navcol = $(".navbar-collapse")
+    colappsed = navcol.hasClass("collapse")
+    if(!colappsed) {
+        navcol.addClass("collapse")
+    }
     arr = nav.toArray()
-
     nav.click(function (e) {
+        if(!navcol.hasClass("collapse")) {
+            navcol.addClass("collapse")
+        }
         i = $(this)
         for (j of nav) {
             let k = $(j)
